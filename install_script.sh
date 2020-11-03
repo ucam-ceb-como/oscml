d755dd051e4abe079a6aad6c6a536eff9f14360b
Daniel Nurkowski <danieln@cmclinnovations.com>
2020-11-03 12:47:14 +0000

    master: Pinned main dependencies in the conda environment.yml file. Improved the install script (hopefully) to not remove the existing conda environment, instead, the environment is now updated. This speeds the installation process.

--- install_script.sh
+++ install_script.sh
@@ -5,54 +5,81 @@
 AUTHOR="Daniel Nurkowski <danieln@cmclinnovations.com>"
 SPATH="$( cd  "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/"
 
-CONDA_ENV_NAME="osc_env"
 DATA_LOCAL="./data/raw"
 DATA_REMOTE="vienna.cheng.cam.ac.uk:/home/userspace/CoMoCommon/Ongoing/Projects/c4e-jps-OSC/Data/Raw/*.*"
+# Read project conda environment name from the environment.yml file
+OLDIFS=$IFS
+IFS=': '
+read tag CONDA_ENV_NAME < "./environment.yml"
+IFS=$OLDIFS
+
+echo
 
 function check_conda {
     echo "1. Verifying conda installation."
+    echo "-------------------------------------------------------------"
     conda_version=$(conda --version)
     if [ $? -eq 0 ]; then
-        echo "Found "$conda_version
+        echo
+        echo "INFO: Found "$conda_version
     else
-        echo "Couldn't find conda installation. On Windows, you must run this script from Anaconda Prompt for conda to be correctly located. Aborting installation."
+        echo "ERROR: Couldn't find conda installation. On Windows, you must run this script from Anaconda Prompt for conda to be correctly located. Aborting installation."
         exit -1
     fi
+    echo
+    echo
 }
 
 function recreate_conda_env {
-    echo "2. Recreating conda project" $CONDA_ENV_NAME "environment (in the default conda environment directory) and installing all necessary dependencies."
-    # firstly remove the environment
-    conda remove -y --name $CONDA_ENV_NAME --all
-    # update the environment name in the yaml file
-    sed -i '1s/.*/name: '$CONDA_ENV_NAME'/' environment.yml
-    # create the environment
-    conda env create -f environment.yml
-}
-
-function get_data_from_server {
-    echo "3. Downloading required project data from the server..."
-    if [ -d $DATA_LOCAL ] 
-    then
-        echo "Directory" $DATA_LOCAL "already exists. Remove it if you wish to re-download data from the server."
-    else
-        mkdir -p $DATA_LOCAL
-        echo -n "Please provide your Vienna username: "
-        read USERNM
-        scp $USERNM"@"$DATA_REMOTE $DATA_LOCAL"/."
+    echo "2. Creating / updating conda project '"$CONDA_ENV_NAME"' environment."
+    echo "-------------------------------------------------------------"
+    # This will create a new environment if one doesn't exist or update the existing one by installing packages listed in the environment.yml file and removing those packaged that are not listed. The environment name will be as specified in the environment.yml file.
+    echo
+    conda env update -f environment.yml --prune
+    if [ $? -ne 0 ]; then
+        echo
+        echo "ERROR: Couldn't create / update conda '"$CONDA_ENV_NAME"' environment. Aborting installation."
+        exit -1
     fi
+    echo
+    echo
 }
 
 function install_ZhouLiML_package {
-    echo "4. Installing the project as a python package..."
+    echo "3. Installing the OSCML project as a python package..."
+    echo "-------------------------------------------------------------"
+    echo
     source activate $CONDA_ENV_NAME
 
     if [ $? -eq 0 ]; then
         pip install -e .
+        if [ $? -ne 0 ]; then
+			echo "ERROR: Couldn't install the OSCML project. Please check the pip log." 
+			exit -1
+        fi
     else
-        echo "Couldn't activate conda" $CONDA_ENV_NAME "environment. Aborting." 
-        exit-1
+        echo "Couldn't activate conda '"$CONDA_ENV_NAME"' environment. Aborting installation." 
+        exit -1
     fi
+    echo
+    echo
+}
+
+function get_data_from_server {
+    echo "4. Downloading required project data from the server..."
+    echo "-------------------------------------------------------------"
+    echo
+    if [ -d $DATA_LOCAL ] 
+    then
+        echo "INFO: Directory" $DATA_LOCAL "already exists. Remove it if you wish to re-download data from the server."
+    else
+        mkdir -p $DATA_LOCAL
+        echo -n "Please provide your Vienna user-name: "
+        read USERNM
+        scp $USERNM"@"$DATA_REMOTE $DATA_LOCAL"/."
+    fi
+    echo
+    echo
 }
 
 if [ "$1" == "-e" ]
@@ -82,10 +109,9 @@ else
     echo "  -e  : re-creates conda environment for this project; it will remove the current '"$CONDA_ENV_NAME"'"
     echo "        environment, if exists, and create it again installing all necessary packages"
     echo "  -i  : installs the oscml project as a python package"
-    echo "  -d  : downloads project data from Vienna (will ask for your Vienna login details)"
-    echo
-    echo
-    echo "==============================================================================================================="
-    echo
+    echo "  -d  : downloads project data from Vienna, (will ask for your Vienna login details)"
 fi
+echo
+echo "==============================================================================================================="
+echo
 read -n 1 -s -r -p "Press any key to continue"
