b42f865c902048bc57de685516925ba6847b385c
Jiaru Bai <jb2197@cam.ac.uk>
2020-12-04 13:30:00 +0000

    dev-only-retrain-best-trial: added codes to plot the regression of measured and predicted PCE values.

--- oscml/visualization/util_sns_plot.py
+++ oscml/visualization/util_sns_plot.py
@@ -1,9 +1,7 @@
-import argparse
-
-import torch
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy import stats
 import matplotlib.pyplot as plt
 
 
@@ -23,64 +21,45 @@ def contour_plot(hpo_result):
     sns_pair_grid.savefig('test_pair_grid.svg')
 
 
-if __name__ == '__main__':
-    #contour_plot('./best_trial_98_svr_hpo_result_trials_part2.csv')
-    contour_plot('./logs/hpo_20201203_232802/hpo_result.csv')
+def init_plot():
+    sns.set(font_scale=1)
+    sns.set_style('whitegrid',
+                  {'axes.grid': True,
+                   'grid.linestyle': u'',
+                   'axes.edgecolor': '0.1',
+                   'axes.labelcolor': '0',
+                   'axes.labelsize': 15,
+                   'axes.titlesize': 15,
+                   'legend.fontsize': 15,
+                   'xtick.labelsize': 15,
+                   'ytick.labelsize': 15,
+                   })
+    plt.rcParams["patch.force_edgecolor"] = True
 
-# def init_plot():
-#     sns.set(font_scale=1)
-#     sns.set_style('whitegrid',
-#                   {'axes.grid': True,
-#                    'grid.linestyle': u'--',
-#                    'axes.edgecolor': '0.1',
-#                    'axes.labelcolor': '0',
-#                    'axes.labelsize': 15,
-#                    'axes.titlesize': 15,
-#                    'legend.fontsize': 15,
-#                    'xtick.labelsize': 15,
-#                    'ytick.labelsize': 15,
-#                    })
-#     plt.rcParams["patch.force_edgecolor"] = True
-#
-#
-# def retrieve_prediction(ckpt_path, model, trainer, train_dl, val_dl, test_dl):
-#     model.load_state_dict(torch.load(ckpt_path)['state_dict'])
-#     model.eval()
-#     training_result = trainer.test(model, test_dataloaders=train_dl)[0]
-#     val_result = trainer.test(model, test_dataloaders=val_dl)[0]
-#     test_result = trainer.test(model, test_dataloaders=test_dl)[0]
-#
-#     return training_result, val_result, test_result
-#
-#
-# def plot_prediction():
-#     init_plot()
-#
-#
-#
-# data_train=pd.DataFrame(predictions_train)
-#
-# g_train = sns.jointplot(x="Train", y="Train_pred", data=data_train,
-#              kind='reg', scatter_kws={'alpha':0.5, 's':20}, height=5)
-#
-# g_train.ax_joint.set_xlabel('Measured Permeability')
-# g_train.ax_joint.set_ylabel('Predicted Permeability')
-# text = "R$^2$ = {:0.2} (Training)".format(r2_train)
-# plt.annotate(text,
-#              xy=(0.1, 0.95),
-#              xycoords='axes fraction')
-# plt.savefig(figures_dir+"{0}_train_reg.svg".format(model_name))
-#
-#
-# data_test=pd.DataFrame(predictions_test)
-#
-# g_test = sns.jointplot(x="Test", y="Test_pred", data=data_test,
-#              kind='reg', scatter_kws={'alpha':0.5, 's':20}, height=5)
-#
-# g_test.ax_joint.set_xlabel('Measured Permeability')
-# g_test.ax_joint.set_ylabel('Predicted Permeability')
-# text = "R$^2$ = {:0.2} (Testset)".format(r2_test)
-# plt.annotate(text,
-#              xy=(0.1, 0.95),
-#              xycoords='axes fraction')
-# plt.savefig(figures_dir+"{0}_test_reg.svg".format(model_name))
+
+def plot(file_path, df):
+    g = sns.jointplot(x="Measured PCE", y="Predicted PCE", data=df,
+                      kind='reg', scatter_kws={'alpha': 0.5, 's': 20}, height=5)
+
+    g.ax_joint.set_xlabel('Measured PCE (%)')
+    g.ax_joint.set_ylabel('Predicted PCE (%)')
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(df['Measured PCE'], df['Predicted PCE'])
+
+    text = "r = {:0.2}".format(r_value)
+    plt.annotate(text,
+                 xy=(0.1, 0.95),
+                 xycoords='axes fraction')
+    plt.savefig(file_path)
+
+
+def prediction_plot(figure_dir, train_pred, val_pred, test_pred):
+    init_plot()
+    # df_results_metric = pd.read_csv(results_metric, index_col=[0])
+    df_train_pred = pd.read_csv(train_pred, index_col=[0])
+    df_val_pred = pd.read_csv(val_pred, index_col=[0])
+    df_train_val = df_train_pred.append(df_val_pred, ignore_index=True, sort=False)
+    df_test_pred = pd.read_csv(test_pred, index_col=[0])
+
+    plot(figure_dir + "/train_val_reg.svg", df_train_val)
+    plot(figure_dir + "/test_reg.svg", df_test_pred)
