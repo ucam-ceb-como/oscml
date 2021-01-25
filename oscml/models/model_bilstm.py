90bbb9ef22e0fd7a42fe9a602afa6aefc6d8266f
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-01-25 16:08:45 +0000

    master: Added comments to the BiLSTM model. Part1.

--- oscml/models/model_bilstm.py
+++ oscml/models/model_bilstm.py
@@ -22,7 +22,7 @@ import oscml.features.weisfeilerlehman
 
 
 class Mol2seq():
-    
+
     def __init__(self, radius, oov, wf=None):
 
         self.radius = radius
@@ -39,14 +39,14 @@ class Mol2seq():
         self.bond_dict = collections.defaultdict(lambda:len(self.bond_dict), bond_dict)
         self.fragment_dict = collections.defaultdict(lambda:len(self.fragment_dict), fragment_dict)
         self.edge_dict = collections.defaultdict(lambda: len(self.edge_dict), edge_dict)
-    
+
         # fragment index starts with 0, thus -1
         self.max_index = len(self.fragment_dict) - 1
         logging.info('initialized Mol2Seq with radius=%s, oov=%s, max_index=%s', radius, oov, self.max_index)
-        
+
     def apply_OOV(self, index):
         return (index if index <= self.max_index else -1)
-        
+
     def __call__(self, m):
         atoms, i_jbond_dict = oscml.features.weisfeilerlehman.get_atoms_and_bonds(m, self.atom_dict, self.bond_dict)
         descriptor = oscml.features.weisfeilerlehman.extract_fragments(self.radius, atoms, i_jbond_dict, self.fragment_dict, self.edge_dict)
@@ -58,7 +58,7 @@ class Mol2seq():
         return descriptor_BFS
 
 class DatasetForBiLstmWithTransformer(torch.utils.data.Dataset):
-    
+
     def __init__(self, df, max_sequence_length, m2seq_fct, padding_index, smiles_fct, target_fct):
         super().__init__()
         self.df = df
@@ -77,7 +77,7 @@ class DatasetForBiLstmWithTransformer(torch.utils.data.Dataset):
             self.target_fct = target_fct
 
         self.smiles2seq = dict()
-    
+
     def __getitem__(self, index):
         row = self.df.iloc[index]
         smiles = self.smiles_fct(row)
@@ -105,7 +105,7 @@ class DatasetForBiLstmWithTransformer(torch.utils.data.Dataset):
         y = torch.as_tensor(np.array(y, dtype=np.float32), device = device)
 
         return [x, y]
-    
+
     def __len__(self):
         return len(self.df)
 
@@ -125,11 +125,11 @@ def get_dataloaders_internal(train, val, test, batch_size, mol2seq, max_sequence
     if test is not None:
         test_ds = DatasetForBiLstmWithTransformer(test, max_sequence_length, mol2seq, padding_index, smiles_fct, target_fct)
         test_dl = torch.utils.data.DataLoader(test_ds, batch_size, shuffle=False)
-   
+
     batch_func = (lambda dl : len(dl) if dl else 0)
     batch_numbers = list(map(batch_func, [train_dl, val_dl, test_dl]))
     logging.info('batch numbers - train val test=%s', batch_numbers)
-    
+
     return train_dl, val_dl, test_dl
 
 
@@ -137,94 +137,162 @@ def get_dataloaders(dataset, df_train, df_val, df_test, transformer, batch_size,
 
     info = oscml.data.dataset.get_dataset_info(dataset)
     mol2seq = info.mol2seq
-    return get_dataloaders_internal(df_train, df_val, df_test, batch_size, mol2seq, max_sequence_length, 
+    return get_dataloaders_internal(df_train, df_val, df_test, batch_size, mol2seq, max_sequence_length,
                 smiles_fct = transformer.transform_x,
                 target_fct = transformer.transform)
 
 
 class Attention(pl.LightningModule):
-    
+
     def __init__(self, vector_dim, sub_fragment_context_vector_dim):
+        # inputs:
+        # vector_dim - dimension of the input vectors sequence to the attention layer, here equal to 2 * hidden vector dimension of lstm
+        #              [ (h1L , h1R) , (h2L , h2R), ..., (h60L , h60R)] for CEPDB, and then each (hiL , hiR) concatenated vector has the
+        #              size as defined by the vector_dim
+        # sub_fragment_context_vector_dim - this is u_sub in Zhou Li paper, must be of the same dimension (this param is then no needed)
+
         super().__init__()
-        
+
         # equation (11) - (13) from paper [Wu20]
-        # "u_sub is the sub-fragment context vector. 
-        # It is randomly initialized and 
-        # jointly learned during the network training process, 
+        # "u_sub is the sub-fragment context vector.
+        # It is randomly initialized and
+        # jointly learned during the network training process,
         # so are the other involved vectors, W_sub and b_sub"
- 
+
         # W_sub and b_sub from eq. (11)
         self.linear = nn.Linear(vector_dim, sub_fragment_context_vector_dim)
         # tanh from eq. (11)
         self.tanh = nn.Tanh()
         # dot product from eq. (12)
-        
+
         initialized_u_sub_tensor = torch.nn.init.normal_(torch.Tensor(sub_fragment_context_vector_dim))
         self.u_sub = nn.parameter.Parameter(initialized_u_sub_tensor)
-        # softmax from eq. (12) over the dim t ('time') of the sequence 
+        # softmax from eq. (12) over the dim t ('time') of the sequence
         # the sum expression of eq. (12) is not correct, it should be
-        #     sum_i exp(u_i^T * u_s)    
+        #     sum_i exp(u_i^T * u_s)
         # i.e. u_i^T instead of u_t^T
-        self.softmax = nn.Softmax(dim=1) 
-    
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, h):
-        
+
         # example: h with batch size 2, max sequence length 3 and vector dim 256
         # h.size() = [2, 3, 256]
         #print('MY ATT INPUT H', h.size(), h)
-        
+
         # just for ease, we assume vector_dim = sub_fragment_context_vector_dim,
         # i.e. the matrix W_sub has quadratic shape [256, 256] and b_sub has shape [256]
         x = self.linear(h)
-        
+
         # tanh is applied element-wise and doesn't change the shape
         x = self.tanh(x)
         # x.size() = [2, 3, 256]
+        #
+        # batch size = 2 then we process 2 molecules at the same time
+        # 3 - is max_sequence_length
+        # 256 - is the concantenated hiden state of bilstm
+        #
+        #                 1 -- 256
+        # [1 - A, 1 , [ev1 (of size 256)]
+        #         .
+        #         .
+        #         3 , [ev3] ,
+        #
+        # 2 - B, 1 ,  [ev1]
+        #         .
+        #         .
+        #         3 , [ev3] ]
+        #
         #print('MY ATT TANH', x.size(), x)
-        
+
         # u_sub.size() = [256]
         #print('MY AT U_SUB', self.u_sub.size(), self.u_sub)
-        
+
         # dot product u_t * u_sub from equation (12):
         # function torch.dot is not capable for broadcasting and batch-processing
         # thus we have break the dot product into two separate operations:
         # a) element-wise multiplication with broadcasting --> size = [2, 3, 256]
         # b) sum along the last dimension --> size = [2,3]
+        #
+        # e.g for an embedding dim of 10
+        # self.u_sub = [v (size 256)]
         x = torch.sum(self.u_sub * x, dim=2)
+        #
+        # [1 - A, 1, sc1 (scalar value after summation)
+        #         .
+        #         .
+        #         3, sc3 ,
+        #
+        # 2 - B, 1, sc1 ,
+        #         .
+        #         .
+        #        3, sc3 ]
         #print('MY ATT DOT ', x.size(), x)
-        
+
         # softmax respects batch-processing --> alpha.size() = [2,3]
         # i.e. alpha contains two attention vectors with probability weights for t=0,1,2 as elements
-        # example output: 
+        # example output:
         #      MY ATT ALPHA torch.Size([2, 3]) tensor([[0.3276, 0.3442, 0.3282],
         #      [0.3297, 0.3471, 0.3231]], grad_fn=<SoftmaxBackward>)
         alpha = self.softmax(x)
+        # [1 - A, 1, alpha1 (scalar value after summation)
+        #         .
+        #         .
+        #         3, alpha3 ,
+        #
+        # 2 - B, 1, alpha1 ,
+        #         .
+        #         .
+        #        3, alpha3 ]
         #print('MY ATT ALPHA', alpha.size(), alpha)
-      
+
         # unsqueeze(dim=2) means: add an "empty" dim at the end --> [2,3,1]
+        # [1 - A, 1, alpha1, (scalar value after summation to a vector with a single element)
+        #         .
+        #         .
+        #         3, alpha3 ,
+        #
+        # 2 - B, 1, alpha1 ,
+        #         .
+        #         .
+        #        3, alpha3 ]
         # elementwise multiplication with the orinal h from the input --> [2, 3, 256]
-        m = alpha.unsqueeze(dim=2) * h    
+        m = alpha.unsqueeze(dim=2) * h
         #print('MY ATT M', m.size(), m)
-        
+
         # sum along sequence index t, i.e. along the dim=1 --> [2,256]
         msum = torch.sum(m, dim=1)
+        # [1 - A, [ev_summed],
+        #
+        # 2 - B, [ev_summed]
+        #                       ]
         #print('MY ATT MSUM', msum.size(), msum)
-        
-        return msum
-    
-class BiLstmForPce(util_lightning.OscmlModule):
-    
-    def __init__(self, number_of_subgraphs, embedding_dim, lstm_hidden_dim, mlp_units, padding_index, target_mean, target_std, optimizer, mlp_dropouts=None):
 
+        return msum
+
+class BiLstmForPce(util_lightning.OscmlModule):
+
+    def __init__(self, number_of_subgraphs, embedding_dim, lstm_hidden_dim, mlp_units, padding_index, target_mean, target_std, optimizer, mlp_dropouts=None):
+        # inputs:
+        # number_of_subgraphs - nr of fragments in a dictionary after processing the entire dataset sample, e.g. CEPDB (25.000 points) = 56
+        # embedding_dim - dimension of the embedding vectors, each fragment type in a sequence will be represented by such vector
+        # lstm_hidden_dim - dimension of the hidden lstm vectors, hardcoded to be equal to the embedding_dim
+        # mlp_units - a list of mlp units numbers for each mlp hidden layer, the nr of items in that list defines the nr of hidden layers
+        # padding_index - e.g. if we have 3 fragments in a sequence for a given molecule (1-2-3) and max sequence is 5, then the sequence is padded
+        #                 1-2-3-P-P, P it is hardcoded for now to be always zero
+        # target_mean/std - for the Z-transformation
+        # optimizer - optimiser settings
+        # mlp_dropouts - a list of dropouts to use in each hidden mlp layer
+        #
         super().__init__(optimizer, target_mean, target_std)
         logging.info('initializing %s', locals())
 
         self.save_hyperparameters()
-        
+
         assert len(mlp_units) > 0
-        
+
         # we add +1 to number_of_subgraphs because
         # padding_idx = 0 in a sequences is mapped to zero vector
+        # the extra 0-index vector is used for out of vocabulary fragment or for padded entries
         self.embedding = nn.Embedding(number_of_subgraphs+1, embedding_dim, padding_idx=padding_index)
         self.bilstm = nn.LSTM(input_size=embedding_dim, hidden_size=lstm_hidden_dim, bidirectional=True)
         # factor 2 because the LSTM is birectional
@@ -235,32 +303,32 @@ class BiLstmForPce(util_lightning.OscmlModule):
         mlp_units_with_input_dim.extend(mlp_units)
         self.mlp = oscml.utils.util_pytorch.create_mlp(mlp_units_with_input_dim, mlp_dropouts)
 
-    
+
     def forward(self, index_sequences):
-        
+
         # get the sequences of embedding vectors corresponding to subgraph indexes
         x = self.embedding(index_sequences)
-        
-        # h is the sequence of hidden state vectors; 
+
+        # h is the sequence of hidden state vectors;
         # the state vectors from both LSTMs are already concatenated
         h, _ = self.bilstm(x)
         # h has shape [batch size, input sequence length, 256]
         # where 256 = lstm_output_dim = 2 * hidden state vector dim = number neurons in the first MLP layer
         #print('MY H', h.size())
-        
- 
-       
+
+
+
         # 1. sum
         # attention mechanism, here only sum (i.e. multiplication with fixed attention weights = 1)*
         # sum_n=1..60 h_n / 60 --> one vector of size [256]
         # --> [batch size, 256]
         #attention_value_msum = torch.mean(h, dim=1)
-        
+
         # 2. equation (11) - (13) from paper [Wu20]
         attention_value_msum = self.attention(h)
         # --> [batch size, 256]
         #print('MY MSUM', attention_value_msum.size(), attention_value_msum)
-        
+
         o = self.mlp(attention_value_msum)
         # transform from size [batch_size, 1] to [batch_size]
         o = o.view(len(o))
