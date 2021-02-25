eea3d5a7d2480b3aa5bd649385aa78e50dbf716f
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-02-25 14:28:53 +0000

    master: Updated the setup file, providing the main oscml entry point.

--- setup.py
+++ setup.py
@@ -1,42 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    name='oscml', # Required
-    version='1.0.0', # Required    
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='Andreas Eibeck', # Optional
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='',  # Optional
-    license='',
-    long_description=open('README.md').read(), # Optional
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
+    name='oscml',
+    version='1.0.0',
+    author='Andreas Eibeck, Daniel Nurkowski, Angiras Menon, Jiaru Bai',
+    license='MIT',
+    description='Organic solar cells PCE prediction models',
+    long_description=open('README.md').read(),
     packages=find_packages(exclude=("tests")),
-    # Specify which Python versions you support pip install' will check this
-    # and refuse to install the project if the version does not match.
     python_requires='>=3.7, <4',
-    #include_package_data=True,
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    #entry_points={  # Optional
-    #    'console_scripts': [
-    #        #'ccparse=compchemparser:compchemparser.main',
-    #         'ccparse=compchemparser.main:main'
-    #    ],
-    #},
+    entry_points={
+        'console_scripts': [
+             'oscml_run=oscml.hpo.train:start'
+        ],
+    },
 )
