ed7c52e86e533c2ce09bea4d40d238f7362fe4d8
Daniel Nurkowski <danieln@cmclinnovations.com>
2021-03-31 14:16:48 +0100

    oscml_kg: Modified the slurm submit script to reflect the changes in the oscml code. It is also now possible to pass any supported oscml code arguments to it.

--- runml_slurm_csd3.sh
+++ runml_slurm_csd3.sh
@@ -6,9 +6,12 @@ echo
 # Store command-line arguments for passing to Slurm script
 args=$@
 
-
 function usage {
     echo "Usage:"
+    echo
+    echo "./runml_slurm_csd3 (JOB_OPTIONS) [ML_OPTIONS]"
+    echo
+    echo "JOB_OTPIONS:"
     echo "  -c               : config file path                            "
     echo "                                                                 "
     echo "  -t  (hh:mm:ss)   : Estimated total wall-time                   "
@@ -17,8 +20,35 @@ function usage {
     echo "                                                                 "
     echo "  -n               : job name (optional)                         "
     echo "  -h               : Display this help and exit                  "
-    exit -1
+    echo
+    echo "ML_OPTIONS"
+    echo "    Please see the oscml readme"
+    echo
+    echo
+    echo "Example call:"
+    echo
+    echo "         this will become the first <configFilePath>"
+    echo "         argument passed to the ml code"
+    echo "                           |           "
+    echo "                           |  this will be appended to the name"
+    echo "                           |   of slurm stdout/stderr file        this will be passed as"
+    echo "                           |               |                      is to the ml code"
+    echo "                      _____|_______     ___|_____               ______|__"
+    echo "                     /              \  /         \             /         \ "
+    echo " ./runml_slurm_csd3 -c configFilePath -n my_ml_job -t 00:05:00 --trials 20"
+    echo "                                                   \__________/"
+    echo "                                                         |"
+    echo "                        this will be used as slurm job time and also converted to"
+    echo "                        --timeout 300 argument to the ml code (300s = 5* 60s)"
+    echo
+    read -n 1 -s -r -p "Press any key to continue"
+    exit
 }
+
+# for debugging the bash script
+#function testfunc {
+#    printf "%s\n" "$@"
+#}
 
 JOBNAME="ML_run"
 # Scan command-line arguments
@@ -26,29 +56,71 @@ if [[ $# = 0 ]]
 then
    usage
 fi
-while [[ $# > 0 ]]
+
+#----------------------------------------------------------------
+# Process all arguments and split them into args that will be used for sbatch command
+# and args that will be passed to the ml code.
+# Note that the
+# --timeout     argument to the ml code is removed and replaced by the slurm --time
+#               argument, where its value is set as 99% of the slurm job time
+# <configFile>  this ml argument is set via -c argument to this bash script
+#
+i=0
+ORIGINAL_ARGS=("$@")
+ML_ARGS=()
+OTHER_ARGS=()
+while [ $i -lt ${#ORIGINAL_ARGS[@]} ]; do
+  arg=${ORIGINAL_ARGS[$i]}
+  if [ $arg == "-n" ] || [ $arg == "-c" ] || [ $arg == "-t" ] ; then
+    OTHER_ARGS+=($arg)
+    i=$((i + 1)) # careful!  ((i++)) will kill your script if you use "set -e"
+    OTHER_ARGS+=(${ORIGINAL_ARGS[$i]})
+  elif [ $arg == "-h" ]; then
+    OTHER_ARGS+=($arg)
+    i=$((i + 1))
+  elif [ $arg == "--timeout" ]; then
+    i=$((i + 1))
+  else
+    ML_ARGS+=($arg)
+  fi
+  i=$((i + 1)) # careful!  ((i++)) will kill your script if you use "set -e"
+done
+# for debuging
+#echo "ML_ARGS = ${ML_ARGS[@]}; length = ${#ML_ARGS[@]}"
+#echo "OTHER_ARGS = ${OTHER_ARGS[@]}; length = ${#OTHER_ARGS[@]}"
+
+
+#----------------------------------------------------------------
+# now process the JOB_OPTIONS arguments
+i=0
+while [ $i -lt ${#OTHER_ARGS[@]} ];
 do
-key="$1"
+key=${OTHER_ARGS[$i]}
+i=$((i + 1))
 case $key in
     -h)
      usage;;
-    -c) CONFIG=$2; shift;;
-    -t) WALLT=$2; shift;;
-    -n) JOBNAME=$2; shift;;
+    -c)
+        CONFIG=${OTHER_ARGS[$i]};
+        i=$((i + 1));;
+    -t)
+        WALLT=${OTHER_ARGS[$i]};
+        i=$((i + 1));;
+    -n)
+        JOBNAME=${OTHER_ARGS[$i]};
+        i=$((i + 1));;
     *)
-    usage
-    # otherwise do nothing
-    ;;
+    usage;;
 esac
-shift # past argument
 done
 
-# Split time into hh, mm and ss
+#----------------------------------------------------------------
+# calculate the --timeout argument
+# split time into hh, mm and ss
 hms=(${WALLT//:/ })
-
-# Calculate total time in ss, or the timeout parameter
+# calculate total time in ss, or the timeout parameter
 WALLTS=$((3600 * ${hms[0]} + 60 * ${hms[1]} + ${hms[2]}))
-# Make timeout 1% smaller than the total slurm time
+# make timeout 1% smaller than the total slurm time
 WALLTS=$(($WALLTS * 99/100))
 
 
@@ -61,14 +133,19 @@ echo "Notification emails will be sent to: $usremailadr"
 echo "(NB Edit your git config in order to change this.)"
 echo
 
-ml_exec=oscml/hpo/train.py
+ml_exec=oscml/driver.py
 
 echo "Submitting job to Slurm..."
 
 STDOUT1=$JOBNAME"_"slurm.%u.%j.%N.stdout.txt
 STDERR1=$JOBNAME"_"slurm.%u.%j.%N.errout.txt
 
-sbatch --mail-user=$usremailadr --time=$WALLT --output=$STDOUT1 --error=$STDERR1 ./SLURM_runhpo_csd3.sh $ml_exec --config $CONFIG --timeout $WALLTS
+sbatch --mail-user=$usremailadr --time=$WALLT --output=$STDOUT1 --error=$STDERR1 ./SLURM_runhpo_csd3.sh $ml_exec --config $CONFIG --timeout $WALLTS $ML_ARGS
+# for debugging the bash script
+#testfunc --mail-user=$usremailadr --time=$WALLT --output=$STDOUT1 --error=$STDERR1 ./SLURM_runhpo_csd3.sh $ml_exec $CONFIG --timeout $WALLTS ${ML_ARGS[@]}
+
+exit
+
 
 echo "Type \"squeue --jobs=<JOBID>\" or \"squeue -u $USER\" to watch it."
 echo
